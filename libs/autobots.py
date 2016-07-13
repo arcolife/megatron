7ca11e3022463deaeaee69125cfa4c61679261c5
Archit Sharma <arcsharm@redhat.com>
2016-07-13 20:08:16 +0530

    init codebase

---
+++ libs/autobots.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import re
+import json
+import logging
+from collections import defaultdict
+
+logger = logging.getLogger(__name__)
+
+
+def flatten_list(L):
+    v = []
+    [v.extend(x) for x in L]
+    return v
+
+
+Class LoadDatabase(object):
+    """
+    """
+    def __init__(self):
+        pass
+
+
+Class JsonConvertor(object):
+    """
+    """
+    def __init__(self):
+        pass
+
+    
+Class QueryConvertor(object):
+    """
+    """
+    def __init__(self):
+        pass
---
+++ libs/minibots.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+
+Class DatasourceCreator(object):
+    """
+    """
+    def __init__(self):
+        pass
+
+
+Class DashboardGenerator(object):
+    """
+    """
+    def __init__(self):
+        pass
+
+
+Class MetadataHandlerES(object):
+    """
+    """
+    def __init__(self):
+        pass
+
+    
+Class MetadataHandlerGraphite(object):
+    """
+    """
+    def __init__(self):
+        pass
+
